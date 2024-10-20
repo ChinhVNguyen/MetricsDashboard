c8406b45bb3af7cba2e1730d7cd1b4ea3d87a526
ChinhVNguyen <chinhfptusa@gmail.com>
2024-10-20 14:04:06 +0700

    FF

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,16 +1,28 @@
-from flask import Flask, json, render_template, jsonify, request
-from prometheus_flask_exporter import PrometheusMetrics
 import logging
-from jaeger_client import Config
+from flask import Flask, render_template, request, jsonify
 from flask_opentracing import FlaskTracing
+from prometheus_flask_exporter import PrometheusMetrics
+from jaeger_client import Config
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+import pymongo
+from flask_pymongo import PyMongo
 
 logging.basicConfig(level=logging.INFO)
 logging.info("LOGLEVEL INFO")
 
 app = Flask(__name__)
-
 metrics = PrometheusMetrics(app)
-metrics.info("app_info", "Backend Application", version="1.0.0")
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+app.config["MONGO_DBNAME"] = "example-mongodb"
+app.config[
+    "MONGO_URI"
+] = "mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb"
+
+mongo = PyMongo(app)
 
 def init_tracer(service):
     config = Config(
